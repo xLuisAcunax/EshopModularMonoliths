c4e4506555b15b087f589cfe7b0074ae35413b6f
Luis Acuña <ldacuna@gmail.com>
2026-05-13 16:24:15 -0500

    Product Event Handlers created

---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Products.EventHandlers
+{
+    public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger)
+        : INotificationHandler<ProductPriceChangedEvent>
+    {
+        public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}

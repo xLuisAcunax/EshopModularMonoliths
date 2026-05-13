1080b83cd6671f907bde33ee5f85541c7fef37cd
Luis Acuña <ldacuna@gmail.com>
2026-05-12 19:00:53 -0500

    api base and docker compose

---
+++ src/Modules/Catalog/Catalog/Products/Events/ProductPriceChangedEvent.cs
@@ -0,0 +1,4 @@
+﻿namespace Catalog.Products.Events
+{
+    public record ProductPriceChangedEvent(Product Product) : IDomainEvent;
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
